7d85953625726d51e91f6c568e26d0cf9a847c16
GI JEONG HONG <hgjwilly@gmail.com>
2025-09-24 11:15:13 +0900

    Initial Commit

---
+++ AIHomeCamProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AIHomeCamProject
+//
+//  Created by 홍기정 on 9/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
